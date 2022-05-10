341011ed42f9210cdbd5a58780922a73bad32105
JiaMing Liu <920817290@qq.com>
2022-05-10 14:09:56 +0800

    2022年5月10日

--- circle.py
+++ circle.py
@@ -3,3 +3,6 @@ import turtle
 turtle.pensize(2)
 turtle.circle(30)
 turtle.circle(40)
+
+name = "你好，我是你爸爸"
+print(name)
---
+++ set.py
@@ -0,0 +1,4 @@
+favorite_language = {'rubin':'python','jack':'cpp','durant':'java'}
+
+for k in sorted(set(favorite_language.keys())):
+    print(k)
